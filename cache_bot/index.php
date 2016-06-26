5fa1cacaeed6e6e38f2644685eaae6af6cf818e5
Fernando Rivers <yayo.fx@gmail.com>
2016-06-25 21:19:02 -0500

    cancel php input

--- cache_bot/index.php
+++ cache_bot/index.php
@@ -3,7 +3,7 @@
 	$botToken = "116742563:AAGE8AXH9uIW91YSkc0lZg2V51ZIyDhQrR4";
 	$website = "https://api.telegram.org/bot".$botToken;
 
-	$update = file_get_contents("php://input");
+	$update = file_get_contents($website."/getupdates");
 	$updateArray = json_decode($update, TRUE);
 
 	$chatId = $updateArray["result"][0]["message"]["chat"]["id"];
