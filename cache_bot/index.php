6895428864d4ab4bf87f37a0650357e400645d18
Fernando Rivers <yayo.fx@gmail.com>
2016-06-25 21:29:17 -0500

    change quotes

--- cache_bot/index.php
+++ cache_bot/index.php
@@ -3,7 +3,7 @@
 	$botToken = "116742563:AAGE8AXH9uIW91YSkc0lZg2V51ZIyDhQrR4";
 	$website = "https://api.telegram.org/bot".$botToken;
 
-	$update = file_get_contents($website."/getupdates");
+	$update = file_get_contents('php://input');
 	$updateArray = json_decode($update, TRUE);
 
 	$chatId = $updateArray["result"][0]["message"]["chat"]["id"];
