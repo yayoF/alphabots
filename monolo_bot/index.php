e6cd3183c1fcca0047f0cfc844f58911262b5e03
Fernando Rivers <yayo.fx@gmail.com>
2016-06-25 22:55:13 -0500

    add funcs

--- monolo_bot/index.php
+++ monolo_bot/index.php
@@ -60,7 +60,7 @@ try {
             $response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
             $response = $client->sendMessage([
                     'chat_id' => $update->message->chat->id,
-                    'text' => "chikicalc: ".$operationString
+                    'text' => chikicalc($operationString)
                 ]);
 
     }
@@ -79,3 +79,11 @@ try {
     //echo $e->getMessage();
 
 }
+
+//private funcs
+public function chikicalc($operationString){
+    $result = 'holi';
+
+    return "chikicalc: ".$result;
+}
+
