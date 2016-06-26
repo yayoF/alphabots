74c57e237741571c4abd85449650492611c18e51
Fernando Rivers <yayo.fx@gmail.com>
2016-06-25 22:26:45 -0500

    new token

--- monolo_bot/index.php
+++ monolo_bot/index.php
@@ -19,10 +19,10 @@
 */
 require 'vendor/autoload.php';
 
-$client = new Zelenin\Telegram\Bot\Api('232355392:AAHs5w6ZiokZx8AXTvx2oHm6nYJJyOzm18I'); // Set your access token
+$client = new Zelenin\Telegram\Bot\Api('232355392:AAG9qESNa-8w4Iz7fAwp1HlWeHcJEAK0C2I'); // Set your access token
 $url = ''; // URL RSS feed
 $update = json_decode(file_get_contents('php://input'));
-print_r($update);
+
 //your app
 try {
 
@@ -39,20 +39,14 @@ try {
     	$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
     	$response = $client->sendMessage([
     		'chat_id' => $update->message->chat->id,
-    		'text' => "List of commands :\n /email -> Get email address of the owner \n /latest -> Get latest posts of the blog \n /help -> Shows list of available commands"
+    		'text' => "List of commands :\n /email -> Get email address of the owner
+             /chikicalc [operation] -> Chiki performs fast calculations
+             /help -> Shows list of available commands"
     		]);
 
     }
-    else if($update->message->text == '/latest')
+    else if($update->message->text == '/chikicalc')
     {
-    		Feed::$cacheDir 	= __DIR__ . '/cache';
-			Feed::$cacheExpire 	= '5 hours';
-			$rss 		= Feed::loadRss($url);
-			$items 		= $rss->item;
-			$lastitem 	= $items[0];
-			$lastlink 	= $lastitem->link;
-			$lasttitle 	= $lastitem->title;
-			$message = $lasttitle . " \n ". $lastlink;
 			$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
 			$response = $client->sendMessage([
 					'chat_id' => $update->message->chat->id,
