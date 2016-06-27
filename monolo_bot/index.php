2724d34119975eba23a1a68172dbe568fc78db4d
Fernando Rivers <yayo.fx@gmail.com>
2016-06-27 16:06:40 -0500

    last else bugging

--- monolo_bot/index.php
+++ monolo_bot/index.php
@@ -68,13 +68,13 @@ try {
     }
     else
     {
-        
+        /*
     	$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
     	$response = $client->sendMessage([
     		'chat_id' => $update->message->chat->id,
     		'text' => "Invalid command, please use /help to get list of available commands"
     		]);
-            
+          */  
     }
 
 } catch (\Zelenin\Telegram\Bot\NotOkException $e) {
