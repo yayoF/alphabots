bfb6a4dd5f65c0b0f29f9abc93314e42980a13c0
yayo <yayo.fx@gmail.com>
2017-09-15 02:29:11 -0500

    test

--- chimpokobot/index.php
+++ chimpokobot/index.php
@@ -70,7 +70,7 @@ try {
         $response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
         $response = $client->sendMessage([
             'chat_id' => $update->message->chat->id,
-            'text' => "lalala"
+            'text' => $yayOperate->getChiste();
             ]);
 
     }
